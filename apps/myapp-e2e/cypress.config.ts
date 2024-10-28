a958746df45c7565d4092e7e5cf38eaa65202233
Miroslav Jonas <missing.manual@gmail.com>
2024-10-28 14:31:40 +0100

    Initial commit

---
+++ apps/myapp-e2e/cypress.config.ts
@@ -0,0 +1,19 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'yarn nx run myapp:serve',
+        production: 'yarn nx run myapp:preview',
+      },
+      ciWebServerCommand: 'yarn nx run myapp:preview',
+      ciBaseUrl: 'http://localhost:4300',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
